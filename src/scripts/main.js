1d88cf210461bcf3518d07840149c248c2ce7fe6
Maciej <metro1568@gmail.com>
2025-03-21 14:52:02 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
-'use strict';
+function createLabel(forAttribute, textNodeChild) {
+  const label = document.createElement('label');
 
-// write code here
+  label.setAttribute('class', 'field-label');
+  label.setAttribute('for', forAttribute);
+  label.appendChild(document.createTextNode(textNodeChild));
+
+  return label;
+}
+
+const upperFields = [
+  ...document.querySelectorAll('body div form')[0].querySelectorAll('.field'),
+];
+
+const lowerFields = [
+  ...document.querySelectorAll('body div form')[1].querySelectorAll('.field'),
+];
+
+upperFields.forEach((field) => {
+  const fieldId = field.lastElementChild.id;
+  let fieldName = field.lastElementChild.name.match(/([a-z]+|[A-Z]{1}[a-z]*)/g);
+
+  fieldName[0] = fieldName[0].at(0).toUpperCase() + fieldName[0].slice(1);
+  fieldName = fieldName.join(' ');
+
+  field.lastElementChild.setAttribute('placeholder', fieldName);
+
+  const toAdd = createLabel(fieldId, fieldName);
+
+  field.prepend(toAdd);
+});
+
+lowerFields.forEach((field) => {
+  const fieldId = field.lastElementChild.id;
+  let fieldName = field.lastElementChild.name.match(/([a-z]+|[A-Z]{1}[a-z]*)/g);
+
+  fieldName[0] = fieldName[0].at(0).toUpperCase() + fieldName[0].slice(1);
+  fieldName = fieldName.join(' ');
+
+  field.lastElementChild.setAttribute('placeholder', fieldName);
+
+  const toAdd = createLabel(fieldId, fieldName);
+
+  field.prepend(toAdd);
+});
